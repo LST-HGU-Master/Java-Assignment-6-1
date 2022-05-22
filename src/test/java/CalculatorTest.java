87f3e7a892d33ef06334d2a5262feabec3928b64
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-22 22:44:41 +0900

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,23 +7,23 @@ public class CalculatorTest {
     public void testTime()
     {
         // assertion
-        assertEquals(2, Calculator.time(1 , 2));
-        assertEquals(36, Calculator.time(4 , 9));
+        assertEquals(2,  Calculator.time(1 , 2), "積の計算結果が間違っています!");
+        assertEquals(36, Calculator.time(4 , 9), "積の計算結果が間違っています!");
     }
 
     @Test
     public void testDivide()
     {
         // assertion
-        assertEquals(0.5, Calculator.divide(1 , 2), 0.0001f);
-        assertEquals(5.0, Calculator.divide(15 , 3), 0.0001f);
+        assertEquals(0.5, Calculator.divide( 1 , 2), 0.0001f, "商の計算結果が間違っています!");
+        assertEquals(5.0, Calculator.divide(15 , 3), 0.0001f, "商の計算結果が間違っています!");
     }
 
     @Test
     public void testDivideZero()
     {
         // assertion
-        assertEquals(0, Calculator.divide(1 , 0), 0.0001f);
-        assertEquals(0, Calculator.divide(26 , 0), 0.0001f);
+        assertEquals(0, Calculator.divide( 1 , 0), 0.0001f, "商の計算結果が指示と異なります!");
+        assertEquals(0, Calculator.divide(26 , 0), 0.0001f, "商の計算結果が指示と異なります!");
     }
 }
